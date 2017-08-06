0f93048db89ff17fdf4006d2b1f30eb869fcbc9e
Daniel <roca486@gmail.com>
2017-08-06 16:50:58 +0200

    integration tests and api tests

--- src/test/java/com/boot/ShipwreckControllerTest.java
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -9,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -34,6 +36,7 @@ public class ShipwreckControllerTest {
 
         Shipwreck wreck = sc.get(1L);
         verify(shipwreckRepository).findOne(1L);
-        assertEquals(1L, wreck.getId().longValue() );
+//        assertEquals(1L, wreck.getId().longValue() );
+        assertThat(wreck.getId(), is(1L));
     }
 }
